486b7e483fe3fd1545ef615fcf4ffe728d1c47e2
Brian Hayes <Brian.hayes1200@gmail.com>
2022-08-25 13:52:53 -0600

    add information

---
+++ variables.rb
@@ -0,0 +1,12 @@
+program = "BE 2210"
+start_date = "October 10th"
+age = 19
+favorite_number = 6
+random_decimals = 6.54
+i_like_basketball = true
+i_like_bananas = false
+program = "fall"
+start_date = "August 22"
+puts "My name is Brian Hayes"
+x = 10
+puts "The value of x is #{10}"
